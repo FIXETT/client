344c80bcbad652954b3467a40b0d98912c8bb323
YoujungSon <andn1026@gmail.com>
2023-03-09 19:16:47 +0900

    #5 feat: aside 컴포넌트 추가

---
+++ src/types/asset.ts
@@ -0,0 +1,48 @@
+export type postAssetDataType = {
+  name: string;
+  department?: string;
+  product: string;
+  category: string;
+  quantity: number;
+  status?: string;
+  manufacturer?: string;
+  acquisitionDate?: string;
+  note?: string;
+  identifier?: string;
+};
+
+export type patchAssetDataType = {
+  assetNumber: number;
+  name: string;
+  department?: string;
+  product: string;
+  category: string;
+  quantity: number;
+  status?: string;
+  manufacturer?: string;
+  acquisitionDate?: string;
+  note?: string;
+  identifier: string;
+};
+export type subNavListType = {
+  id: string;
+  link: string;
+  title: string;
+};
+export type getAssetListType = {
+  assetNumber: number;
+  name: string;
+  department?: string;
+  product: string;
+  category: string;
+  quantity: number;
+  status?: string;
+  manufacturer?: string;
+  acquisitionDate?: string;
+  note?: string;
+  createdAt: string;
+  identifier: string;
+  updatedAt: string;
+  __v: number;
+  _id: string;
+};
