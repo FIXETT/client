cd68b697adbca5a440732044f1b32b56ada3b6b6
YoujungSon <andn1026@gmail.com>
2023-03-13 17:26:46 +0900

    feat: hotjar 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './router/Router';
 import GlobalStyle from './styles/globalstyle';
+import { hotjar } from 'react-hotjar';
 function App() {
   if (1 === 1) {
     console.log = function no_console() {};
     console.warn = function no_console() {};
   }
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'development') {
+      hotjar.initialize(3403053, 6);
+    }
+  }, []);
+
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
