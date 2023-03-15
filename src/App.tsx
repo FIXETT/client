f093e6db25fcfe565b1f0067f5f28859a03df54c
YoujungSon <andn1026@gmail.com>
2023-03-15 13:48:24 +0900

    feat: react-ga 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './router/Router';
 import GlobalStyle from './styles/globalstyle';
 import { hotjar } from 'react-hotjar';
+import ReactGA from 'react-ga';
 
 function App() {
   const queryClient = new QueryClient();
@@ -12,7 +13,14 @@ function App() {
     console.log = function no_console() {};
     console.warn = function no_console() {};
   }
-
+  const pathName = window.location.pathname;
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.initialize('UA-260316176-6');
+      ReactGA.set({ page: pathName });
+      ReactGA.pageview(pathName);
+    }
+  }, [pathName]);
   useEffect(() => {
     if (process.env.NODE_ENV !== 'development') {
       hotjar.initialize(3403053, 6);
