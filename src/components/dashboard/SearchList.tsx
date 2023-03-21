7263853d76dc644c064b01b35f6767e65096c4a9
YoujungSon <andn1026@gmail.com>
2023-03-21 14:13:37 +0900

    fix: 검색페이지 오류 수정

--- src/components/dashboard/SearchList.tsx
+++ src/components/dashboard/SearchList.tsx
@@ -20,10 +20,10 @@ const SearchList = ({ assetList }: Props) => {
       const searchProduct = assetList?.filter((value) => String(value.product).includes(searchText));
       const searchCategory = assetList?.filter((value) => String(value.category).includes(searchText));
       const searchDepartment = assetList?.filter((value) => String(value.department).includes(searchText));
-      const searchManufacturer = assetList?.filter((value) => value.manufacturer.includes(searchText));
-      const searchacAuisitionDate = assetList?.filter((value) => value.acquisitionDate.includes(searchText));
+      const searchManufacturer = assetList?.filter((value) => String(value.manufacturer).includes(searchText));
+      const searchacAuisitionDate = assetList?.filter((value) => String(value.acquisitionDate).includes(searchText));
       const searchStatus = assetList?.filter((value) => String(value.status).includes(searchText));
-      const searchNote = assetList?.filter((value) => value.note.includes(searchText));
+      const searchNote = assetList?.filter((value) => String(value.note).includes(searchText));
       const result = [
         ...searchName,
         ...searchProduct,
