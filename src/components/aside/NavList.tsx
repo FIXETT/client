c4378068c01416f581dc964855580bb99b245ed8
YoujungSon <andn1026@gmail.com>
2023-04-27 19:15:12 +0900

    #5 feat: aside 수정

--- src/components/aside/NavList.tsx
+++ src/components/aside/NavList.tsx
@@ -14,23 +14,11 @@ const NavList = () => {
   const match = useLocation().pathname;
   const [subToggleMeru, setToggleMenu] = useState(false);
 
-  const TogglesubMeru = () => {
-    setToggleMenu(!subToggleMeru);
-  };
-
   const navList = [
     { id: '1', link: 'assetList', title: '자산 리스트', img: AssetList },
     { id: '2', link: 'dashboard', title: '자산 대쉬보드', img: Dashboard },
     { id: '3', link: 'fix', title: '자산 수리', img: Fix },
-    // {
-    //   id: '4',
-    //   title: '마이페이지',
-    //   img: mypage,
-    //   subNavList: [
-    //     { id: '1', link: 'mypage', title: '내 정보 수정하기' },
-    //     { id: '2', link: 'addemployee', title: '직원 초대하기' },
-    //   ],
-    // },
+    { id: '4', link: 'mypage', title: '마이페이지', img: Mypage },
   ];
 
   return (
@@ -38,35 +26,13 @@ const NavList = () => {
       {navList.map((value) => {
         return (
           <ListItemContainer key={value.id}>
-            {/* {!value?.subNavList ? ( */}
             <ListItemLink match={String(match === `/${value.link}`)} to={value.link}>
               <value.img stroke={String(match === `/${value.link}`) === 'true' ? '#000' : '#999'} />
               <p>{value.title}</p>
             </ListItemLink>
-            {/* ) : (
-          <ListItemContainer onClick={TogglesubMeru}>
-            <ListItem>
-              <img src={arrow} alt="화살표" />
-              <ListItemImg src={value.img} alt="아이콘" />
-              <p>{value.title}</p>
-            </ListItem>
-            <SubList subToggleMeru={subToggleMeru}>
-              {value?.subNavList.map((subValue: subNavListType) => (
-                <ListItemContainer key={subValue.id}>
-                  <ListItemLink match={String(match === `/${subValue.link}`)} to={subValue.link}>
-                    <p>{subValue.title}</p>
-                  </ListItemLink>
-                </ListItemContainer>
-              ))}
-            </SubList>
-          </ListItemContainer>
-        )} */}
           </ListItemContainer>
         );
       })}
-      {/* <BlankLink to="https://forms.gle/5Fh5WeSV2ErDcF9k7" target="_blank">
-        수리 요청하기
-      </BlankLink> */}
     </>
   );
 };
